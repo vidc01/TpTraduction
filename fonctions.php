65b6313abf1c77aa2db39571f893de2b29dc0a9f
hchikatabdelrahmane-ops <hchikatabdelrahmane@gmail.com>
2025-11-16 13:39:21 +0100

    Ajout des fonctions traduction

--- fonctions.php
+++ fonctions.php
@@ -15,3 +15,39 @@ function normaliserMot($mot) {
 
     return mb_strtolower($mot, "UTF-8");
 }
+function obtenirMotsConnus($sens) {
+    $dictionnaire = obtenirDictionnaire();
+
+    if ($sens === "versAnglais") {
+        return array_keys($dictionnaire); 
+    }
+
+    if ($sens === "versFrancais") {
+        return array_values($dictionnaire); 
+    }
+
+    return [];
+}
+
+function traduireMot($mot, $sens) {
+    $mot = normaliserMot($mot);
+    $dictionnaire = obtenirDictionnaire();
+
+    if ($sens === "versAnglais") {
+
+        return $dictionnaire[$mot] ?? null;
+    }
+
+    if ($sens === "versFrancais") {
+
+        foreach ($dictionnaire as $fr => $en) {
+            if ($mot === normaliserMot($en)) {
+                return $fr;
+            }
+        }
+        return null;
+    }
+
+    return null;
+}
+?> 
